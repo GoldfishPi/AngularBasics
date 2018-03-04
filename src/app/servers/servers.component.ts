49457ca2e5f4b9b4d222b8082473a4248024fab5
GoldfishPi <erikbadger777@gmail.com>
2018-03-04 11:31:38 -0600

    Styled server component and made servers component dynamic

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,17 +4,31 @@ import { Component, OnInit } from '@angular/core';
   //selector: '[app-servers]',
   //selector:'.app-servers',
   selector:'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'no server creation';
+  serverName:string = 'TestServerName';
+  serverCreated:boolean = false;
+  servers = ['TestServer','TestServer2'];
 
-  constructor() { }
+  constructor() {
+    setTimeout(()=> {
+      this.allowNewServer=true;
+    },2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer(){
+    this.serverCreationStatus = 'server was created! Name is ' + this.serverName;
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
+  }
+  onUpdateServerName(event:any){
+      this.serverName = event.target.value;
+  }
 }
